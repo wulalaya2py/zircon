a1d95ee401d780c45978b8e3ff02293921a42e38
Yvonne Yip <yky@google.com>
2018-03-06 13:31:46 -0800

    [DO NOT CHECK IN] intel adsp firmware files
    Firmware files:
    
    * dsp_fw_kbl_v3266.bin - ADSP firmware
    * 9d71-GOOGLE-EVEMAX-0-tplg.bin - topology file for Pixelbook
    * max98927-render-2ch-48khz-24b.bin - I2S config for speaker
    
    Change-Id: I33ef2b89f132e8db2f5a7cf9bb3346ee0b00a3ba

--- system/dev/audio/intel-hda/rules.mk
+++ system/dev/audio/intel-hda/rules.mk
@@ -8,3 +8,16 @@ MODULES += \
     $(LOCAL_DIR)/controller \
     $(LOCAL_DIR)/codecs/qemu \
     $(LOCAL_DIR)/codecs/realtek \
+
+ADSP_FIRMWARE_NAME := dsp_fw_kbl_v3266.bin
+ADSP_FIRMWARE_BIN := $(LOCAL_DIR)/firmware/$(ADSP_FIRMWARE_NAME)
+
+ADSP_TPLG_NAME := 9d71-GOOGLE-EVEMAX-0-tplg.bin
+ADSP_TPLG_BIN := $(LOCAL_DIR)/firmware/$(ADSP_TPLG_NAME)
+
+ADSP_I2S_CFG_NAME := max98927-render-2ch-48khz-24b.bin
+ADSP_I2S_CFG_BIN := $(LOCAL_DIR)/firmware/$(ADSP_I2S_CFG_NAME)
+
+USER_MANIFEST_LINES += {core}lib/firmware/$(ADSP_FIRMWARE_NAME)=$(ADSP_FIRMWARE_BIN)
+USER_MANIFEST_LINES += {core}lib/firmware/$(ADSP_TPLG_NAME)=$(ADSP_TPLG_BIN)
+USER_MANIFEST_LINES += {core}lib/firmware/$(ADSP_I2S_CFG_NAME)=$(ADSP_I2S_CFG_BIN)
