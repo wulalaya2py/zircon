ace9e3cdb240e7eb0f6d4513b372e1edc5ce51f2
Yvonne Yip <yky@google.com>
2018-04-04 19:49:39 -0700

    [dev][intel-audio-dsp] initialize basic DSP pipeline
    Initialize a single pipeline for speaker playback.
    
    Change-Id: I7eea457e4a53a804942924b90c1bbf35b833a5f1

--- system/dev/audio/intel-hda/dsp/rules.mk
+++ system/dev/audio/intel-hda/dsp/rules.mk
@@ -10,8 +10,10 @@ MODULE_TYPE := driver
 
 MODULE_SRCS += \
     $(LOCAL_DIR)/binding.c \
+    $(LOCAL_DIR)/debug.cpp \
     $(LOCAL_DIR)/intel-audio-dsp.cpp \
     $(LOCAL_DIR)/intel-dsp-code-loader.cpp \
+    $(LOCAL_DIR)/intel-dsp-ipc.cpp \
 
 MODULE_LIBS := \
     system/ulib/driver \
@@ -20,13 +22,15 @@ MODULE_LIBS := \
 
 MODULE_STATIC_LIBS := \
     system/ulib/audio-driver-proto \
-    system/ulib/ddk \
-    system/ulib/intel-hda \
     system/ulib/audio-proto-utils \
+    system/ulib/ddk \
+    system/ulib/ddktl \
     system/ulib/dispatcher-pool \
     system/ulib/fbl \
+    system/ulib/intel-hda \
+    system/ulib/pretty \
+    system/ulib/sync \
     system/ulib/zx \
     system/ulib/zxcpp \
-    system/ulib/ddktl \
 
 include make/module.mk
